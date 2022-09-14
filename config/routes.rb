72d877934262cc2aa19939d8288f5c6b267fd1e9
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-09-14 12:42:56 +0000

    [Add]一覧表示でのedit,destroyの表示＆リンクの作成一部完了

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 
-  resources :books, only: [:index, :create, :show]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 
   get "/homes/about" => "homes#about", as: "about"
 
