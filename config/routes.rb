8f8e4ecb0f0d6af433a0c8a2704919b57e72d9b6
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-09-16 13:48:29 +0000

    [Add]10.userのcontrollerファイルの作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
-
-  get "/homes/about" => "homes#about", as: "about"
+  resources :users, only: [:show, :edit]
 
 end
