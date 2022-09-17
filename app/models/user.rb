6379b2b13cd12985f12a35c3bc5624ff90cddb9a
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-09-17 14:34:01 +0000

    [Add]フラッシュメッセージとバリデーションの実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 50}
+  validates :introduction, length: {maximum: 50}
+
   def get_profile_image(width,height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -16,5 +19,6 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit:[width,height]).processed
   end
+
 
 end
