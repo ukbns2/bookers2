85d0d31ff8912d061cbf5f88ab72afd28e569ccb
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-09-17 06:47:26 +0000

    [Add]フラッシュメッセージとバリデーションの実装一部（新規投稿＆更新)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     render :index
+    end
   end
 
 
@@ -25,8 +30,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to '/books'
+    if book.update(book_params)
+      flash[:notice] ="You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
 
@@ -42,6 +52,8 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+
 
 end
 
