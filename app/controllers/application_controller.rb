e76b30460f217535f95a2c8667245a018d92c7a1
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-09-18 09:25:24 +0000

    [Add]投稿に失敗した際のエラーメッセージの表示設定完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top, :about]
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, uniqueness: true, length: {minimum: 2, maximum: 50}
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
   validates :introduction, length: {maximum: 50}
 
   def get_profile_image(size)
